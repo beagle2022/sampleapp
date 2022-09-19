c298a742f12da1519f75846ea231da2adc8e2f0e
Abhishek Bhattacharya <ab7607@rediffmail.com>
2022-09-19 10:50:47 +0530

    Added interpolation from circle to triangle

--- codingtrain1/sketch.js
+++ codingtrain1/sketch.js
@@ -1,6 +1,9 @@
 let cirPath=[];
 let triPath=[];
+//let morPath=[];
 let spacing=10;
+let theta=0;
+
 
 function polarToCartesian(r,angle){
 	return createVector(r*cos(angle),r*sin(angle));
@@ -14,17 +17,16 @@ function setup() {
 	let endA=120;
 	let start=polarToCartesian(radius,startA);
 	let end=polarToCartesian(radius,endA);
-
-	for(let a=startA;a<360;a+=spacing){
-		let x=radius * cos(a);
-		let y=radius * sin(a);
+	for(let a=startA; a<360; a+=spacing){
 		let cv=polarToCartesian(radius,a);
 		cirPath.push(cv);
-		let amt=a/(endA-startA);
+		let amt=(a%120)/(endA-startA);
 		let tv=p5.Vector.lerp(start,end,amt);
 		triPath.push(tv);
+		console.log(a%120);
 
-		if(a%120===0){
+		if((a + spacing) % 120 === 0){
+			console.log("oops");
 			startA+=120;
 			endA+=120;
 			start=polarToCartesian(radius,startA);
@@ -32,33 +34,53 @@ function setup() {
 		}	
 	}
 
-
-
-
 }
 
 function draw() {
 	// put drawing code here
 	background(220);
 	translate(width/2, height/2);
+	rotate(30);
 	stroke(0);
 	noFill();
+	let amt=(sin(theta)+1)/2;
+	theta+=5;
 	beginShape();
-	for(let i=0;i<cirPath.length;i++){
+	for(i=0;i<cirPath.length;i++){
+
+		let cv=cirPath[i];
+		let tv=triPath[i];
+
+		let x=lerp(cv.x,tv.x,amt);
+		let y=lerp(cv.y,tv.y,amt);
+		vertex(x,y);
+	}
+
+
+
+	//Circle
+/* 	for(let i=0;i<cirPath.length;i++){
 		let v=cirPath[i];
 		vertex(v.x,v.y);
-	}
+	} 
 
-	endShape();
-
-	beginShape();
-	for(let i=0;i<triPath.length;i++){
+	//little points/* 
+	for(i=0;i<triPath.length;i++){
 		let v=triPath[i];
-		vertex(v.x,v.y);
-	}
+		ellipse(v.x,v.y,8);
+	} */
 
-	endShape();
+	endShape(CLOSE);
+	
+/* 	//Triangle
+	beginShape();
+		for(let i=0;i<triPath.length;i++){
+			let v=triPath[i];
+			vertex(v.x,v.y);
+		}
 
+	endShape(CLOSE);
+ */
 
 
 
